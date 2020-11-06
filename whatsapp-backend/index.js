9b618777f64447c62ade6994f9ce63dad311960e
unknown <punithmatli@gmail.com>
2020-11-06 06:22:52 +0530

    Created Database schema with message, name and timestamp

--- whatsapp-backend/index.js
+++ whatsapp-backend/index.js
@@ -1,16 +1,39 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import Messages from './dbmessages.js'
 
 //app config
 const app = express();
 const port = process.env.PORT || 9000
 
 //middlewares
+app.use(express.json());
 
 //DB config
+
+const connection_url = `mongodb+srv://admin:b43h3hOCvMxWUXxe@cluster0.gzukk.mongodb.net/whatsappdb?retryWrites=true&w=majority`
+mongoose.connect(connection_url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex:true
+})
+
 
 //apis
 app.get('/',(req, res) => {
     return res.status(200).send('hello world')
+})
+
+app.post('/messages/new', (req,res) => {
+    const dbMessage = req.body;
+    Messages.create(dbMessage, (err , data) => {
+        if(err) {
+            res.status(500).send(err);
+        }
+        else {
+            res.status(201).send(`New message created: ${data}`)
+        }
+    })
 })
 
 //listener
