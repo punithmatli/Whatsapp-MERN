0a77590e3a855b46e9896b20b36172df53723ca0
unknown <punithmatli@gmail.com>
2020-11-07 15:46:30 +0530

    Implemented 'Type and Send' functionality using the input text box in chat footer

--- whatsapp-ui/src/components/Chat/Chat.js
+++ whatsapp-ui/src/components/Chat/Chat.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Avatar, IconButton } from "@material-ui/core";
 
 import './Chat.css'
 import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@material-ui/icons';
+import axios from '../../axios'
 
-const Chat = ({messages}) => {
+const Chat = ({ messages }) => {
+    const [input, setInput] = useState("");
+
+    const sendMessage = async (e) => {
+        e.preventDefault();
+        await axios.post('/messages/new', {
+            message:input,
+            name: "Name",
+            timestamp:"Just now",
+            received:false
+        })
+        setInput('');
+    }
+
     return (
         <div className="chat">
             <div className="chat__header">
@@ -30,7 +44,7 @@ const Chat = ({messages}) => {
 
             <div className="chat__body">
                 {messages.map(message => (
-                        <p className={`chat__message ${message.received && `chat__receiver`}`}>
+                    <p className={`chat__message ${message.received && `chat__receiver`}`}>
                         <span className="chat__name">{message.name}</span>
                         {message.message}
                         <span className="chat__timestamp">
@@ -42,11 +56,14 @@ const Chat = ({messages}) => {
 
             <div className="chat__footer">
                 <InsertEmoticon />
-                <form>
-                    <input placeholder="Type a Message" type="text" />
+                <form onSubmit={sendMessage}>
+                    <input value={input}
+                        onChange={e => setInput(e.target.value)}
+                        placeholder="Type a Message"
+                        type="text" />
                     <button type="submit">Send a message</button>
                 </form>
-								<Mic />
+                <Mic />
             </div>
 
         </div>
